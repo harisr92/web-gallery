5ca713c11d6da68b2ddb0459968f2b5929d2b60e
harisr92 <harikrishnansr92@live.com>
2016-08-22 15:52:08 +0530

    Signup for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   before_save :downcase
+  has_attached_file :avatar, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg"
+  validates_attachment_content_type :avatar, content_type: %w(image/jpeg image/jpg image/png)
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 250},
@@ -8,7 +10,6 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
-
   private
     def downcase
       self.email.downcase
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822084738) do
+ActiveRecord::Schema.define(version: 20160822094449) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "password_digest"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
