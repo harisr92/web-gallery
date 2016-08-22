7d900c0b04cdb978a85ace9da93f4f0e8bb174a5
harisr92 <harikrishnansr92@live.com>
2016-08-22 16:34:54 +0530

    Add login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save :downcase
-  has_attached_file :avatar, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg"
+  has_attached_file :avatar, :styles => { :medium => "200x200", :thumb => "100x100" }, :default_url => "missing.jpg"
   validates_attachment_content_type :avatar, content_type: %w(image/jpeg image/jpg image/png)
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
